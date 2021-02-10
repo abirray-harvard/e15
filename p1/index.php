d0eae0066227fcdf999f97cde546beef2fa5d5c8
Abir Ray <abirray-harvard@gmail.com>
2021-02-09 19:24:04 -0500

    <feat>: added vowelCount function

--- p1/index.php
+++ p1/index.php
@@ -3,14 +3,26 @@
 function isPalindrome($inputStr)
 {
     $str = preg_replace('/[^a-z]/i','',strtolower($inputStr));
-    if(strcmp($str,strrev($str))!=0) return false;
-    else return true;
+    if(strcmp($str,strrev($str))!=0) return "No";
+    else return "Yes";
 }
 
-echo isPalindrome("racecar")==true?"P":"N";
-echo isPalindrome("Racecar")==true?"P":"N";
-echo isPalindrome("racecar!")==true?"P":"N";
-echo isPalindrome("!racecar!")==true?"P":"N";
-echo isPalindrome("Hello World")==true?"P":"N";
+echo "<br>Test: isPalindrome<br>";
+echo "racecar => " . isPalindrome("racecar") . "<br>";
+echo "Racecar => " . isPalindrome("Racecar") . "<br>";
+echo "racecar! => " . isPalindrome("racecar!") . "<br>";
+echo "!racecar! => " . isPalindrome("!racecar!") . "<br>";
+echo "Hello World => " . isPalindrome("Hello World") . "<br>";
+
+function vowelCount($inputStr)
+{
+    $str = preg_replace('/[^aeiou]/i','',$inputStr);
+    return strlen($str);
+}
+
+echo "<br>Test: vowelCount<br>";
+echo "the quick brown fox jumps over the lazy dog => " . vowelCount("the quick brown fox jumps over the lazy dog") . "<br>";
+echo "Hll Wrld => " . vowelCount("Hll Wrld") . "<br>";
+echo "AeIoU => " . vowelCount("AeIoU") . "<br>";
 
 require 'index-view.php';
